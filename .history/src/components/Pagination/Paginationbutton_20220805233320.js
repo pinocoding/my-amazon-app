c68cc74e960daad69c4e6b11dcd6a2d04e29a4ec
Pino trying to code <88932667+Pinocoding@users.noreply.github.com>
2022-08-06 00:53:24 +0700

    fixing something

---
+++ .history/src/components/Pagination/Paginationbutton_20220805233320.js
@@ -0,0 +1,160 @@
+import React from "react";
+import firebase from "firebase/compat/app";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+function Paginationbutton({ page, list, setList, setPage, col, selected }) {
+  let field;
+  let sorting;
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const showNext = ({ item }) => {
+    if (selected == 0) {
+      field = "price";
+      sorting = "asc";
+    }
+    if (selected == 1) {
+      field = "price";
+      sorting = "asc";
+    }
+    if (selected == 2) {
+      field = "price";
+      sorting = "desc";
+    }
+
+    // if (list.length === 0) {
+    //   alert("Thats all we have for now !");
+    // } else {
+    const fetchNextData = async () => {
+      await firebase
+        .firestore()
+        .collection(col)
+        .orderBy(field, sorting)
+        .limit(8)
+        .startAfter(item.price)
+        .onSnapshot(function (querySnapshot) {
+          const items = [];
+          querySnapshot.forEach(function (doc) {
+            items.push({ key: doc.id, ...doc.data() });
+          });
+
+          setList(items);
+          setPage(page + 1);
+        });
+    };
+    fetchNextData();
+    }
+    if (selected == 3) {
+      const fectchingOnsales = async () => {
+        await firebase
+          .firestore()
+          .collection(col)
+          .where("onsales", "==", "best price")
+          .orderBy("price", "desc")
+          .limit(8)
+          .startAfter(item.onsales)
+          .onSnapshot(function (querySnapshot) {
+            const items = [];
+            querySnapshot.forEach(function (doc) {
+              items.push({ key: doc.id, ...doc.data() });
+            });
+            setList(items);
+            setPage(page + 1);
+
+          });
+      };
+      fectchingOnsales();
+    }
+    console.log(list);
+  };
+  const showPrevious = ({ item }) => {
+    const fetchPreviousData = async () => {
+      if (selected == 0) {
+        field = "price";
+        sorting = "asc";
+      }
+      if (selected == 1) {
+        field = "price";
+        sorting = "asc";
+      }
+      if (selected == 2) {
+        field = "price";
+        sorting = "desc";
+      }
+
+      await firebase
+        .firestore()
+        .collection(col)
+        .orderBy(field, sorting)
+        .endBefore(item.price)
+        .limitToLast(8)
+        .onSnapshot(function (querySnapshot) {
+          const items = [];
+          querySnapshot.forEach(function (doc) {
+            items.push({ key: doc.id, ...doc.data() });
+          });
+          setList(items);
+          setPage(page - 1);
+        });
+    };
+    // fetchPreviousData();
+    // if (selected == 3) {
+    //   const fectchingOnsales = async () => {
+    //     await firebase
+    //       .firestore()
+    //       .collection(col)
+    //       .where("onsales", "==", "best price")
+    //       .orderBy("price", "desc")
+    //       .limit(8)
+    //       .endBefore(item.price)
+    //       .onSnapshot(function (querySnapshot) {
+    //         const items = [];
+    //         querySnapshot.forEach(function (doc) {
+    //           items.push({ key: doc.id, ...doc.data() });
+    //         });
+
+    //         setList(items);
+    //         setPage(page - 1);
+    //       });
+    //   };
+    //   fectchingOnsales();
+    // }
+  }
+;
+  return (
+    <div className="pagination__container">
+      {
+        //show previous button only when we have items
+        page === 1 ? (
+          ""
+        ) : (
+          <button
+            className="pagination__button"
+            onClick={() => showPrevious({ item: list[0] })}
+          >
+            <ArrowBackIosNewIcon />
+          </button>
+        )
+      }
+      {
+        //show next button only when we have items
+        list.length < 8 ? (
+          ""
+        ) : (
+          <button
+            className="pagination__button"
+            onClick={() => showNext({ item: list[list.length - 1] })}
+          >
+            <ArrowForwardIosIcon className="pagination__icon" />
+          </button>
+        )
+      }
+    </div>
+  );
+}
+
+export default Paginationbutton;
---
+++ .history/src/components/Products/OptionLeft_20220805235552.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./OptionLeft.css";
+function OptionLeft({ selected, handleChange }) {
+  return (
+    <div>
+      <select className="sort__column" value={selected} onChange={handleChange}>
+        <option value="0">Sorted by Featured</option>
+        <option value="1">Price: Low to High</option>
+        <option value="2">Price: High to Low</option>
+        <option value="3">Hot</option>
+      </select>
+    </div>
+  );
+}
+
+export default OptionLeft;
