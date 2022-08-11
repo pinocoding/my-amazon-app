f6553823fd0587e019db5c09dd7efaa7e1451072
Pino trying to code <88932667+Pinocoding@users.noreply.github.com>
2022-08-11 19:33:49 +0700

    Authentication

---
+++ .history/src/components/Payment/Payment_20220811182050.js
@@ -0,0 +1,51 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import "./Payment.css";
+import { DataContext } from "../DataLayer/Datalayer";
+import CheckoutProduct from "../CheckOut/CheckoutProduct";
+
+function Payment() {
+  const context = useContext(DataContext);
+  const totalQty = context.totalQty;
+  const currentUserEmail = context.currentUserEmail;
+  const currentUser = context.currentUserName;
+  const userCartProduct = context.userCartProduct;
+  // console.log(userCartProduct);
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout(<Link to="/checkout">{totalQty}-item</Link>)
+        </h1>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery address </h3>
+          </div>
+          <div className="payment__address">
+            <p>User: {currentUser}</p>
+            <p>Email: {currentUserEmail}</p>
+            <p>address*****</p>
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {/*  */}
+            <CheckoutProduct userCartProduct={userCartProduct} />
+            {/*  */}
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">{/* stripe */}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
