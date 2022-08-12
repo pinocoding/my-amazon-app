c5726b716a04590e41fcf0e6549c1af186644648
Pino trying to code <88932667+Pinocoding@users.noreply.github.com>
2022-08-12 23:58:32 +0700

    fixed

---
+++ .history/src/components/CarouselForm/TopToys_20220812231900.js
@@ -0,0 +1,62 @@
+import React, { useContext } from "react";
+import { DataContext } from "../DataLayer/Datalayer";
+import { Link } from "react-router-dom";
+import "./Carousel.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import Slider from "react-slick";
+
+function TopToys(props) {
+  const numbers = props.numbers;
+  const settings = {
+    // dots: true,
+    className: "center",
+    autoplay: false,
+    speed: 3000,
+    autoplaySpeed: 5000,
+    autoPlay: true,
+    infinite: true,
+
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
+  const context = useContext(DataContext);
+  const isDark = context.isDark;
+  return (
+    <div className="container">
+      <div className="topsellercarousel">
+        <Slider {...settings}></Slider>
+      </div>
+    </div>
+  );
+}
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+export default TopToys;
