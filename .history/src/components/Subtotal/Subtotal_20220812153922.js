bfb309f0fc3023796e9a9091f9a94cf20c191066
Pino trying to code <88932667+Pinocoding@users.noreply.github.com>
2022-08-12 20:22:24 +0700

    Login with GG

---
+++ .history/src/components/Subtotal/Subtotal_20220812153922.js
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import CurrencyFormat from "react-currency-format";
+import "./Subtotal.css";
+import { DataContext } from "../DataLayer/Datalayer";
+
+function Subtotal() {
+  const context = useContext(DataContext);
+  const isDark = context.isDark;
+  const currentUser = context.currentUser;
+  const userCartProduct = context.currentUser;
+  const totalPrice = context.totalPrice;
+  const totalQty = context.totalQty;
+
+  return (
+    <div className={`subtotal ${isDark ? "subtotal-dark" : "subtotal-light"}`}>
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal&nbsp;
+              <span className="subtotal__count">({totalQty} items)</span>
+              &nbsp;: <strong>{value}</strong>
+            </p>
+            {/* <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small> */}
+          </>
+        )}
+        decimalScale={2}
+        value={totalPrice}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      {currentUser && userCartProduct.length > 0 && (
+        <button>
+          <Link to="/payment">
+            Proceed to Checkout {console.log(currentUser, userCartProduct)}
+          </Link>
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Subtotal;
